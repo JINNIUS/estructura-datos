fcad205d3c131ec9dbda853f8c9a0a4674d103c2
unknown <pedromanelromero@gmail.com>
2024-02-22 11:08:40 -0600

    22/02/2024

--- EstructurasLineales/Ejercicios/FrameEL.py
+++ EstructurasLineales/Ejercicios/FrameEL.py
@@ -8,28 +8,41 @@ class FrameEL(tk.Frame):
         self.ordenes = []
 
         self.texto_label = tk.Label(self, 
-                 text="Orden Actual:")
+                 text="Orden actual:")
         self.texto_label.pack()
 
         self.texto_var = tk.StringVar()
         self.texto_label_actual = tk.Label(
             self, textvariable=self.texto_var)
         self.texto_label_actual.pack()
-        
+
         self.boton_opcion1 = tk.Button(
-            self,
-            text="Opcion 1",
+            self, 
+            text="Opción 1",
             command=self.agregar_opcion1)
         self.boton_opcion1.pack()
 
+        self.boton_opcion2 = tk.Button(
+            self, 
+            text="Opción 2",
+            command=self.agregar_opcion2)
+        self.boton_opcion2.pack()
+    
     def agregar_opcion1(self):
-        texto_nuevo = "Opcion 1"
+        texto_nuevo = 'Opcion 1 '
+        self.ordenes.append(texto_nuevo)
+        self.actualizar_orden()
+
+    def agregar_opcion2(self):
+        texto_nuevo = 'Opcion 2 '
         self.ordenes.append(texto_nuevo)
         self.actualizar_orden()
 
     def actualizar_orden(self):
-        texto_actual = "".join(self.ordenes)
+        texto_actual = ''.join(self.ordenes)
         self.texto_var.set(texto_actual)
+
 root = tk.Tk()
 editor_frame = FrameEL(master=root)
-editor_frame.mainloop()
+editor_frame.pack()
+root.mainloop()
